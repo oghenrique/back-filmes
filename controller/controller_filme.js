1d56efbc634e89871939eb5bf296fd798c8a9b7a
Gustavo <santosguhenrique@gmail.com>
2024-01-30 17:18:23 -0300

    iniciando DAO

---
+++ controller/controller_filme.js
@@ -0,0 +1,39 @@
+/**********************************************************************************************************
+ * Objetivo: Arquivo responsável por realizar validações, consistencias e regra de negócio para os filmes *
+ * Data: 30/01/2024                                                                                       *
+ * Autor: Gustavo Henrique                                                                                *
+ * 1.0                                                                                                    *
+ *********************************************************************************************************/
+
+//Função para inserir um novo filme
+const setNovoFilme = async () => {
+
+}
+
+//Função para atualizar um filme existente
+const setAtualizarFilme = async () => {
+
+}
+
+//Função para excluir um Filme existente
+const setExcluirFilme = async () => {
+
+}
+
+//Função para retornar todos os filmes do database
+const getListarFilmes = async () => {
+
+}
+
+//Função para retornar filtro do filme pelo ID
+const getBuscarFilme = async () => {
+
+}
+
+module.exports = {
+    setNovoFilme,
+    setAtualizarFilme,
+    setExcluirFilme,
+    getListarFilmes,
+    getBuscarFilme
+}
---
+++ model/DAO/filme.js
@@ -0,0 +1,39 @@
+/**********************************************************************************************************
+ * Objetivo: Arquivo responsável por realizar CRUD no Banco de Dados MySQL                                *
+ * Data: 30/01/2024                                                                                       *
+ * Autor: Gustavo Henrique                                                                                *
+ * 1.0                                                                                                    *
+ *********************************************************************************************************/
+
+//Função para inserir um filme no Banco de Dados
+const insertFilme = async () => {
+
+}
+
+//Função para atualizar um filme no Banco de Dados
+const updateFilme = async () => {
+
+}
+
+//Função para deletar um filme no Banco de Dados
+const deleteFilme = async () => {
+
+}
+
+//Função para retornar todos os filmes do Banco de Dados
+const selectAllFilmes = async () => {
+
+}
+
+//Função para buscar um filme no Banco de Dados filtrando pelo ID
+const selectByIdFilme = async () => {
+
+}
+
+module.exports{
+    insertFilme,
+    updateFilme,
+    deleteFilme,
+    selectAllFilmes,
+    selectByIdFilme
+}
